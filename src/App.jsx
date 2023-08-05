ea3d29ba6c6d89b6734605d10071a5d777eef5c1
javier <javierbernadasback@gmail.com>
2023-08-05 19:13:41 +0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div className=" w-full h-screen  "> 
+        <h1 className="   text-center" > Javierr Bernadas Canoy</h1>
+
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
